7ecea926a2ad94a3f211a3167b5681274bc391a5
MuscularSloth <39010838+MuscularSloth@users.noreply.github.com>
2021-12-07 10:24:35 +0300

    HomeComponent Add New (#3)
    * HomeComponent Add New
    
    * second commit

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HomeComponent() {
+    return (
+        <div>
+           <p>Hello HomeComponent, this is best component I have ever seen!</p>
+        </div>
+    )
+}
