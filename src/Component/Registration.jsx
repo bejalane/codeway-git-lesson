0b985d402cace493322488de6399fc7e69b41710
RudeAnna <87259884+RudeAnna@users.noreply.github.com>
2021-12-07 09:26:22 +0200

    add registration component (#4)
    * add registration component
    
    * add text

---
+++ src/Component/Registration.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Registration = () => {
+    return (
+        <div>
+            <p>REGISTRATION</p>
+            <p>some more text</p>
+        </div>
+    )
+}
+
+export default Registration
